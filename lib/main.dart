dd3a19146975efd843919e704057a3f64f0ff791
bosenija <nijatechmob@gmail.com>
2026-02-27 13:48:44 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'main'),
     );
   }
 }
@@ -120,3 +120,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+ 
